c5f13732e0732bf968f08df489fbabc75597e92d
Devastati0n <havokcs1@yahoo.com>
2024-08-02 19:22:48 -0700

    update2

--- server.js
+++ server.js
@@ -13,14 +13,20 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views'); 
 app.set('layout', 'layouts/layout'); 
 app.use(expressLayouts); 
-app.use(express.static('public')); 
+app.use(express.static('public'))
 
 const mongoose = require('mongoose'); 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL); 
 
 const db = mongoose.connection; 
 db.on('error', error => console.error(error)); 
 db.once('open', () => console.log('connected')); 
+
+
+
+
+
+
 
 
 
@@ -43,9 +49,16 @@ app.listen(process.env.PORT || 3000);
 npm i express ejs express-ejs-layouts
 npm i --save-dev nodemon
 npm i mongoose
+git init 
+
+git remote add origin https://github.com/Devastati0n/full.git
+git branch -M main
+git push -u origin main
+
+Set-ExecutionPolicy –ExecutionPolicy RemoteSigned
 */ 
 
 
-
-
+//PIurGCPggKnLLI7U
+//mongodb+srv://havokcs1:<PIurGCPggKnLLI7U>@cluster0.hzry53b.mongodb.net/
 
