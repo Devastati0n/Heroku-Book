43a2b2884681de71db796029ebb38ec95a7c9fdd
Devastati0n <havokcs1@yahoo.com>
2024-08-05 00:03:41 -0700

    hello

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENV !=='production'){
+if(process.env.NODE_ENV !== 'production'){
 require("dotenv").config()
 
 }
@@ -15,12 +15,14 @@ app.set('layout', 'layouts/layout');
 app.use(expressLayouts); 
 app.use(express.static('public'))
 
+
 const mongoose = require('mongoose'); 
-mongoose.connect(process.env.DATABASE_URL); 
+mongoose.connect(process.env.DATABASE_URL);
+
 
 const db = mongoose.connection; 
 db.on('error', error => console.error(error)); 
-db.once('open', () => console.log('connected')); 
+db.once('open', () => console.log('connected to mongoose')); 
 
 
 
@@ -50,6 +52,7 @@ npm i express ejs express-ejs-layouts
 npm i --save-dev nodemon
 npm i mongoose
 git init 
+npm run devStart
 
 git remote add origin https://github.com/Devastati0n/full.git
 git branch -M main
