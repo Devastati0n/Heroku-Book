2d0ff692f29840cda7ebd0bf6455d4b782a815f9
Devastati0n <havokcs1@yahoo.com>
2024-08-14 21:41:46 -0700

    deploy12

--- routes/books.js
+++ routes/books.js
@@ -3,15 +3,14 @@ const router = express.Router();
 const multer = require('multer'); 
 const path = require('path')
 const Book = require('../models/book')
-const uploadPath = path.join('public', Book.coverImageBasePath)
 const Author = require('../models/author');
-const author = require('../models/author');
+const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 
 const upload = multer({
     dest: uploadPath,
     fileFilter:(req,file,callback) =>{
-        callback(null, )
+        callback(null, imageMimeTypes.includes(file.mimetype))
     }
 })
 
@@ -27,7 +26,7 @@ router.get('/new', async (req,res)=>{
 })
 
 //create book route
-router.post('/', async (req,res)=> {
+router.post('/', upload.single('cover'), async (req,res)=> {
 const fileName = req.file != null ? req.file.filename : null 
 const book = new Book({
     title:req.body.title, 
